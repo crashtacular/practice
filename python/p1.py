cafd65adcc2faa7af142abe7ce158bd05914eea4
Eric Sweigart <eric.sweigart@nextest.com>
2019-10-20 16:32:59 -0700

    temp commit

---
+++ python/p1.py
@@ -0,0 +1,31 @@
+print ("Hello World!")
+
+if True:
+    print ("True")
+else:
+    print ("False")
+    
+    
+item = 5 +\
+    6 +\
+    7
+    
+print(item)
+
+days = ['Monday','Tuesday','Wednesday','Thursday','Friday']
+
+#Data types:
+v1 = 1
+v2 = 1.01
+v3 = 'String'
+v4 = [1,2,3,'hello',5,6]# different data types
+v5 = ['T1,T2,T3']
+v6 = [(1,2,'COmbo'),(2,3,'Word'),('ex',2,3)] #read only
+dict = {}
+dict[1] = "One"
+dict['two'] = 2
+print(dict[1])
+print(dict['two'])
+
+print(str(10000))
+print(repr(10101010))
